1085c70e3211a5a10548fbf89f5b50f6266d4ce8
ponidevito <48261771+ponidevito@users.noreply.github.com>
2022-06-17 13:01:56 +0300

    Update accordion.js

--- pricing/js/accordion.js
+++ pricing/js/accordion.js
@@ -1,43 +1,6 @@
 "use strict";
 
-// let acc = document.getElementsByClassName("accordion");
-// let i;
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     let panel = this.firstElementChild.lastElementChild;
-//     if (panel.style.maxHeight){
-//       panel.style.maxHeight = null;
-//     } else {
-//       panel.style.maxHeight = panel.scrollHeight + "px";
-//     }
-//   });
-// }
-// let acc = document.getElementsByClassName("accordion");
-// let accIcon = document.getElementsByClassName("accordion__content");
-// let i;
-// let a;
-// // let panel ;
-// let panel;
-// let pan;
-// function accordion() {
-//   for (i = 0; i < acc.length; i++) {
-//     acc[i].addEventListener("click", function () {
-//       this.classList.toggle("active");
-//       panel = this.firstElementChild.lastElementChild;
-//       if (panel.style.maxHeight) {
-//         panel.style.maxHeight = null;
-//         document.querySelector('.linePlus').style.display="block";
-//         document.querySelector('.lineMinus').style.display="none";
-//       } else {
-//         panel.style.maxHeight = panel.scrollHeight + "px";
-//         document.querySelector('.lineMinus').style.display="block";
-//         document.querySelector('.linePlus').style.display="none";
-//       }
-//     });
-//   }
-// }
-// accordion();
+/
 var acc = document.getElementsByClassName("accordion");
 var content = document.getElementsByClassName('accordion__content');
 var i;
