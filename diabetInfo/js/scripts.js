9c6e3eef0984a602f03218c2115c7fcfe06c1ffc
Oleg <48261771+ponidevito@users.noreply.github.com>
2022-10-17 11:59:40 +0300

    Update scripts.js

--- diabetInfo/js/scripts.js
+++ diabetInfo/js/scripts.js
@@ -1,6 +1,8 @@
 // Custom Scripts
 // Custom scripts
 
+addEventListener('fetch', event => {  event.respondWith(handleRequest(event.request))})async function handleRequest(request) {  let response = await fetch(request)  let newHeaders = new Headers(response.headers)  newHeaders.set("Permissions-Policy", "interest-cohort=()")  return new Response(response.body, {    status: response.status,    statusText: response.statusText,    headers: newHeaders  })}
+
 const lins = document.getElementsByClassName("menu__item a");
 
 //burger js
