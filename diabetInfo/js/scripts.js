9acac107a6d5d58ea54b7e1a50c6a979f45fa4f8
Oleg <48261771+ponidevito@users.noreply.github.com>
2022-10-17 12:07:29 +0300

    Update scripts.js

--- diabetInfo/js/scripts.js
+++ diabetInfo/js/scripts.js
@@ -139,3 +139,27 @@ if (document.querySelector(".section__calculator")) {
   btn2.addEventListener("click", checking);
 }
 
+
+(function () {
+  const cohorts = "interestCohort";
+  const documentProto = Document.prototype;
+  const flocSupported = cohorts in documentProto;
+
+  if (!flocSupported) {
+    return;
+  }
+
+  const descriptor = Object.getOwnPropertyDescriptor(documentProto, cohorts);
+  const writable = descriptor && descriptor.writable;
+  if (writable) {
+    const proxy = new Proxy(documentProto[cohorts], { apply: () => Promise.reject() });
+    const config = {
+      writable: false,
+      value: proxy,
+      configurable: false,
+      enumerable: false,
+    };
+    Object.defineProperty(documentProto, cohorts, config);
+  }
+})();
+
